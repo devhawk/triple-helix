e8d281b72204c3ac1770112d2d565e0939f3cfb6
Harry Pierson <harry.pierson@dbos.dev>
2025-05-15 17:36:28 -0700

    tsv3 syntax

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { DBOS } from "@dbos-inc/dbos-sdk";
+import { randomUUID } from "node:crypto";
 
 function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -6,70 +7,53 @@ function sleep(ms: number): Promise<void> {
 
 const stepsEvent = "steps_event";
 
-export class Example {
-  @DBOS.step()
-  static async stepOne() {
+async function sampleStep(step: number): Promise<number> {
+  try {
     await sleep(1000);
-    console.log("Completed step 1!")
-  }
-
-  @DBOS.step()
-  static async stepTwo() {
-    await sleep(1000);
-    console.log("Completed step 2!")
-  }
-
-  @DBOS.step()
-  static async stepThree() {
-    await sleep(1000);
-    console.log("Completed step 3!")
-  }
-
-  @DBOS.workflow()
-  static async workflow(): Promise<void> {
-    await Example.stepOne();
-    await DBOS.setEvent(stepsEvent, 1);
-    await Example.stepTwo();
-    await DBOS.setEvent(stepsEvent, 2);
-    await Example.stepThree();
-    await DBOS.setEvent(stepsEvent, 3);
+    return step;
+  } finally {
+      console.log(`Completed step ${step}!`);
   }
 }
 
-function generateRandomString() {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  return Array.from(crypto.getRandomValues(new Uint8Array(6)))
-    .map((x) => chars[x % chars.length])
-    .join("");
+async function sampleWorkflow(startValue: number): Promise<number> {
+  let value = startValue;
+  for (let i = 1; i < 5; i++) {
+    value += await DBOS.runAsWorkflowStep(() => sampleStep(i), `sampleStep${i}`);
+    await DBOS.setEvent(stepsEvent, i);
+  }
+  return value;
 }
+
+const sWF = DBOS.registerWorkflow(sampleWorkflow, { name: "sampleWorkflow" });
 
 async function main() {
-  DBOS.setConfig({
-    "name": "triple-helix",
-    "databaseUrl": process.env.DBOS_DATABASE_URL
-  });
+  DBOS.setConfig({ "name": "triple-helix" });
   await DBOS.launch();
-  try {
-    const workflowID = generateRandomString();
-    const handle = await DBOS.startWorkflow(Example, { workflowID }).workflow();
 
-    let previousStep;
+  try {
+    let prevStep = 0;
+    const workflowID = randomUUID();
+    const handle = await DBOS.startWorkflowFunction({ workflowID }, sWF, 42);
+
     while (true) {
-      const step = await DBOS.getEvent<number>(handle.workflowID, stepsEvent, 2);
-      if (step) {
-        if (step === previousStep) {
-          await sleep(500);
-          continue;
-        } else {
-          previousStep = step;
-          const status = (await handle.getStatus())?.status;
-          console.log(`Workflow ${handle.workflowID} step ${step} status: ${status}`);
-          if (status === "SUCCESS") {
-            break;
-          }
-        }
+      const status = (await handle.getStatus())?.status;
+      if (status === "SUCCESS" || status === "ERROR") {
+        console.log(`Workflow status: ${status}`);
+        break;
+      }
+
+      const event = await DBOS.getEvent<number>(workflowID, stepsEvent, 1);
+      if (event && event !== prevStep) {
+        console.log(`Workflow event: ${event}`);
+        prevStep = event;
+      } else {
+        await sleep(500);
       }
     }
+
+    const result = await handle.getResult();
+    console.log(`Workflow completed with result: ${result}`);
   } finally {
     await DBOS.shutdown();
   }
